ba89c293494e9ee5176e01f4587a13b51d78c32f
Benjamin Soutendijk <devaent1316@gmail.com>
2017-07-05 20:56:47 +0000

    added Gems, deleted routes, deleted index page pending rework

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   
-  
   root to: 'articles#index'
-
-
 
   resources :articles
 
-
-
-  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
 
-
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
