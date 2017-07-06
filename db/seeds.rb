1dfb8444bfa31c22349d7341e56eadcf75c51045
Benjamin Soutendijk <devaent1316@gmail.com>
2017-07-06 11:45:20 +0000

    seeded database, changed index.html to more concise code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(0..50).each do |i|
+    Article.create(title: 'Article #{i}', text: 'Lorem ipsum')
+end
