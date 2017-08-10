8c656a7fe75f61d1096928e269d57da4c6014fa8
Benjamin Soutendijk <devaent1316@gmail.com>
2017-08-10 18:35:45 +0000

    completed structure of homepage with index of article thumbnails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# (1..5).each do |i|
-#     Article.create(title: "Article #{i}", text: 'Lorem ipsum')
-# end
+(1..5).each do |i|
+    Article.create!(title: "Article #{i}", text: 'Lorem ipsum', average_color: "#000000")
+end
