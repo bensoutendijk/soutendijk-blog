bbd48bb4b292944b66d3e51a5b917f63249d2ae1
Benjamin Soutendijk <devaent1316@gmail.com>
2017-08-15 21:00:19 +0000

    deleted more cropper stuff, played around with article index page controller, helper, and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,42 @@
 class ArticlesController < ApplicationController
+  
+  
+  def index
+    @article = Article.all
+    # Count the number of articles to display
+    a = @article.count%3 #And store modulo 3
     
-    
-    def index
-        @article = Article.all
+    # Articles are displayed in rows alternating between 1 and 2 thumbnails per row
+    case a
+      when 0..1
+        # If the number of articles mod 3 is 1 or 0, then the first row contains one thumbnail
+        # This means that the counter is set to 2
+        @counter = 2
+      when 2
+        # Else if the number of articles%3 is 2, then the first row contains two thumbnails
+        # This means the counter should be set to 0
+        @counter = 0
     end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+    @article = Article.new(article_params)
     
-    def show
-        @article = Article.find(params[:id])
+    @article.save
+    redirect_to @article
+  end
+  
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :text, :average_color, :crop_x, :crop_y, :crop_w, :crop_h, :thumbnail)
     end
-    
-    def new
-    end
-    
-    def create
-        @article = Article.new(article_params)
-        
-        @article.save
-        redirect_to @article
-    end
-    
-    
-    private
-        def article_params
-            params.require(:article).permit(:title, :text, :average_color, :crop_x, :crop_y, :crop_w, :crop_h, :thumbnail)
-        end
-    
+  
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,3 +1,27 @@
 module ArticlesHelper
-    
+  
+  def article_html (article, offset=0, fullwidth=true)
+    if fullwidth
+      size = 12
+    else
+      size = 6
+    end
+    pid = article.id
+    offset = pid - 1 - offset
+    # Store data for offset article
+    id = @article[offset].id
+    title = @article[offset].title
+    thumbnail_file_name = @article[offset].thumbnail_file_name
+    average_color = @article[offset].average_color
+    # Create the article html
+    link_to article_path(article) do
+      content_tag :div, :class => "col-md-#{size} homepage-thumbnail text-center", style: "background: #{average_color};" do
+        content_tag :h2, "#{id}", :class => "thumbnail-hover"
+        if !thumbnail_file_name.nil?
+          thumbnail_file_name.gsub(/.jpg/, ".png")
+          image_tag "articles/#{id}/homepage/#{thumbnail_file_name}"
+        end
+      end
+    end
+  end
 end
