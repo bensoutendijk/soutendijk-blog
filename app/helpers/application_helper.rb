1e39eff2c21a0cbdaaa48a18d416cbc148980670
Benjamin Soutendijk <devaent1316@gmail.com>
2017-08-21 14:57:35 +0000

    made nav-bar active classes dryer, prettier.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,3 +8,13 @@ def flash_class(level)
         when 'alert' then "container alert alert-dismissable alert-danger"
     end
 end
+def nav_link(link_text, link_path, on_the_right = false)
+    
+    if on_the_right
+    class_name = current_page?(link_path) ? 'navbar-text navbar-link navbar-right navbar-link-active' : 'navbar-text navbar-link navbar-right'
+    else
+    class_name = current_page?(link_path) ? 'navbar-text navbar-link navbar-link-active' : 'navbar-text navbar-link'
+    end
+    link_to link_text, link_path, class: class_name
+end
+    
