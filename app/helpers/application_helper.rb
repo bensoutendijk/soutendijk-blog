4bf228c1fd773a89af624109f7f19807eb503f9d
Benjamin Soutendijk <devaent1316@gmail.com>
2017-09-29 18:39:45 +0000

    added geese

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,12 @@
 module ApplicationHelper
-    
-end
-def flash_class(level)
+  
+  def flash_class(level)
     case level
-        when 'notice' then "flash alert alert-info"
-        when 'success' then "flash alert alert-success"
-        when 'danger' then "flash alert alert-danger"
-        when 'alert' then "flash alert alert-danger"
+      when 'notice' then "flash alert alert-info"
+      when 'success' then "flash alert alert-success"
+      when 'danger' then "flash alert alert-danger"
+      when 'alert' then "flash alert alert-danger"
     end
-end
+  end
 
-def get_all_articles
-    @all_articles = Article.all
-end
-
-def get_all_users
-    @all_users = User.all
 end
