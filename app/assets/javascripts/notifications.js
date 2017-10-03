dc07c2ce753586d90ceb6d894ba4f1ef460da494
Benjamin Soutendijk <devaent1316@gmail.com>
2017-10-03 23:25:40 +0000

    prepping for bootstrap

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -4,17 +4,8 @@
 
 $(document).on('turbolinks:load', function(){
   
-  
   $('#flash-container').animate({
     'top': '62px'
   }, 134);
-    
-  setTimeout(function(){
-    
-    $('#flash-container').animate({
-      'top': '-3px'
-    }, 134);
-    
-  }, 6000);
   
 });
