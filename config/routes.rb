19934873de8762bb73b3d4c556f2922a15c71584
Benjamin Soutendijk <devaent1316@gmail.com>
2017-10-03 23:34:05 +0000

    back to the basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  mount Ckeditor::Engine => '/ckeditor'
-  root to: 'articles#index'
+  # devise_for :users
+  # mount Ckeditor::Engine => '/ckeditor'
+  root to: 'pages#home'
 
-  resources :articles do
-    resources :comments, module: :articles, only: [:create, :destroy]
-    resources :votes, module: :articles, only: [:create, :destroy, :update]
-    patch :toggle_hidden, on: :member
-  end
+  # resources :articles do
+  #   resources :comments, module: :articles, only: [:create, :destroy]
+  #   resources :votes, module: :articles, only: [:create, :destroy, :update]
+  #   patch :toggle_hidden, on: :member
+  # end
   
-  resources :comments do
-    resources :votes, module: :comments, only: [:create, :destroy, :update]
-  end
+  # resources :comments do
+  #   resources :votes, module: :comments, only: [:create, :destroy, :update]
+  # end
   
   get 'about', to: 'pages#about'
-  get 'settings', to: 'pages#settings'
 
 end
