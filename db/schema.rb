a4404e61578080b5813031c27f51886ec57a0bcf
Benjamin Soutendijk <devaent1316@gmail.com>
2017-11-03 00:59:36 +0000

    created controllers, model, and view for profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024150818) do
+ActiveRecord::Schema.define(version: 20171103005159) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,22 @@ ActiveRecord::Schema.define(version: 20171024150818) do
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
